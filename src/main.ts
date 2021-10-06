93b3755dc35cc30b1017efca19749c8695103fd2
simon.kvasnovsky@gmail.com <simon.kvasnovsky@gmail.com>
2021-10-06 20:57:36 +0200

    Snail

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,7 @@
-const message: string = 'Hello AppsLab 2021!';
-console.log(message);
+let heightOfStep : number = 0.3;
+let lenghtOfStep : number = 0.2;
+let towerHeight : number = 25;
+let numberOfSteps : number = (towerHeight/heightOfStep);
+let result : number = numberOfSteps* (heightOfStep+lenghtOfStep);
+var distance: string = "Total distance is " + result;
+console.log(distance);
