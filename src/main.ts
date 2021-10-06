d205b9a2df97a7c209fe03c7360e1fbc6a8fd8b3
simon.kvasnovsky@gmail.com <simon.kvasnovsky@gmail.com>
2021-10-06 21:08:12 +0200

    triangle

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
-console.log("Output: ");
+
 let num1 : number = 8; 
 let num2 : number = 4; 
 let num3 : number = 8; 
-if (num1 == num2 && num1 == num3)
+if (num1+num2>num3 && (num1+num3>num2) && (num3+num2>num1))
         {
-            console.log("3 numbers are equal");
-        }
-        else if ((num1 == num2) || (num1 == num3) || (num3 == num2))
-        {
-            console.log("2 numbers are equal");
+            console.log("It is a triangle");
         }
         else
         {
-            console.log("0 numbers are equal1");
+            console.log("It is not a triangle");
         }
