fc882808a91827462a0613b9bccdc3cba90d024c
simon.kvasnovsky@gmail.com <simon.kvasnovsky@gmail.com>
2021-10-06 21:04:23 +0200

    equalityOf3numbers

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
-let heightOfStep : number = 0.3;
-let lenghtOfStep : number = 0.2;
-let towerHeight : number = 25;
-let numberOfSteps : number = (towerHeight/heightOfStep);
-let result : number = numberOfSteps* (heightOfStep+lenghtOfStep);
-var distance: string = "Total distance is " + result;
-console.log(distance);
+console.log("Output: ");
+let num1 : number = 8; 
+let num2 : number = 4; 
+let num3 : number = 8; 
+if (num1 == num2 && num1 == num3)
+        {
+            console.log("3 numbers are equal");
+        }
+        else if ((num1 == num2) || (num1 == num3) || (num3 == num2))
+        {
+            console.log("2 numbers are equal");
+        }
+        else
+        {
+            console.log("0 numbers are equal1");
+        }
