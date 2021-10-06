25a9d3d768202cf918895d99abcca67b7f982379
simon.kvasnovsky@gmail.com <simon.kvasnovsky@gmail.com>
2021-10-06 21:13:50 +0200

    slicesOfPie

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 
-let num1 : number = 8; 
-let num2 : number = 4; 
-let num3 : number = 8; 
-if (num1+num2>num3 && (num1+num3>num2) && (num3+num2>num1))
-        {
-            console.log("It is a triangle");
-        }
-        else
-        {
-            console.log("It is not a triangle");
-        }
+let totalSlices : number = 8; 
+let numberOfPeople : number = 4; 
+let slicesEach : number = 8; 
+if (numberOfPeople*slicesEach<=totalSlices)
+{
+    console.log("(" + totalSlices +","+ numberOfPeople +","+ slicesEach + ")" + " It is true ");
+}
+else {
+    console.log("(" + totalSlices +","+ numberOfPeople +","+ slicesEach + ")" + " It is false ");
+}
