2d59db6290cb9892c635a86ca798b12147985d3a
simon.kvasnovsky@gmail.com <simon.kvasnovsky@gmail.com>
2021-10-08 22:10:06 +0200

    endingStringMatches

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,22 @@
-
-var array = [5, 9, 45, 67, 3, 5, 0, 2,44, 8, 26, 903];
-var even = 0;
-var odd = 0;
-
-for(let i = 0; i < array.length; i++){
-
-    if(array[i] %2 == 0){
-        even += array[i];
+{
+    let one : String = "abc";
+    let bc = one.indexOf("bc");
+    if(bc)
+    {
+        console.log(true);;
     }
-    else{
-        odd += array[i];
+    else
+    {
+        console.log(false);
     }
-}
 
-if(even > odd){
-    console.log("Even > Odd");
-    var a = even - odd;
-    console.log(a);
-}
-else{
-    console.log("Odd > Even");
-    var b = odd - even;
-    console.log(b);
+
+    let two : String = "Car";
+    if(two.indexOf("ar"))
+    {
+        console.log(true);
+    }
+    else {
+        console.log(false);
+    }
 }
