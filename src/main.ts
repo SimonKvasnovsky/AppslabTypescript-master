66bd70c761b33facca9c7f02d3781a83637b136e
simon.kvasnovsky@gmail.com <simon.kvasnovsky@gmail.com>
2021-10-08 22:38:49 +0200

    captureTheRook

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,14 @@
-{
-    let one : String = "abc";
-    let bc = one.indexOf("bc");
-    if(bc)
+function CaptureTheRook(rook:string[]){
+    if(rook[0].charAt(0) == rook[1].charAt(0) || rook[0].charAt(1) == rook[1].charAt(1))
     {
-        console.log(true);;
+        return true;
     }
-    else
-    {
-        console.log(false);
-    }
-
-
-    let two : String = "Car";
-    if(two.indexOf("ar"))
-    {
-        console.log(true);
-    }
-    else {
-        console.log(false);
+    else{
+        return false;
     }
 }
+var rooks1:string[] = ["A8", "E8"];
+var rooks2:string[] = ["B6", "A1"];
+
+ console.log("Result is: "+CaptureTheRook(rooks1));
+ console.log("Result is: "+CaptureTheRook(rooks2));
